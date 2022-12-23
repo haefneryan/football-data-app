ce3fc915e1248986512d493e1681f5305b5cd487
haefneryan <haefner.ryan@gmail.com>
2022-12-23 14:10:55 -0500

    Pages

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,3 +1,5 @@
+import * as ActionTypes from "../actions/actionTypes";
+
 const initState = {
   breadcrumb: ["Home"],
   competitions: [],
@@ -11,12 +13,12 @@ const initState = {
 };
 
 const rootReducer = (state = initState, action) => {
-  if (action.type === "FETCH_COMPS_DATA_SUCCESS") {
+  if (action.type === ActionTypes.FETCH_COMPS_DATA_SUCCESS) {
     return {
       ...state,
       competitions: action.payload.data,
     };
-  } else if (action.type === "SET_ACTIVE_COMP") {
+  } else if (action.type === ActionTypes.SET_ACTIVE_COMP) {
     return {
       ...state,
       activeComp: state.competitions.filter((comp) => {
@@ -34,39 +36,39 @@ const rootReducer = (state = initState, action) => {
         })[0].Name,
       ],
     };
-  } else if (action.type === "SET_ACTIVE_SEASON") {
+  } else if (action.type === ActionTypes.SET_ACTIVE_SEASON) {
     return {
       ...state,
       activeSeason: action.payload,
       breadcrumb: [...state.breadcrumb, action.payload.Name],
     };
-  } else if (action.type === "SET_ACTIVE_SEASON_TEAMS") {
+  } else if (action.type === ActionTypes.SET_ACTIVE_SEASON_TEAMS) {
     return {
       ...state,
       seasonTeams: action.payload,
     };
-  } else if (action.type === "SET_ACTIVE_TEAM") {
+  } else if (action.type === ActionTypes.SET_ACTIVE_TEAM) {
     return {
       ...state,
       activeTeam: action.payload,
       breadcrumb: [...state.breadcrumb, action.payload.TeamName],
     };
-  } else if (action.type === "SET_ACTIVE_SQUAD") {
+  } else if (action.type === ActionTypes.SET_ACTIVE_SQUAD) {
     return {
       ...state,
       squad: action.payload,
     };
-  } else if (action.type === "SET_BREADCRUMB") {
+  } else if (action.type === ActionTypes.SET_BREADCRUMB) {
     return {
       ...state,
       breadcrumb: [...state.breadcrumb, action.payload],
     };
-  } else if (action.type === "REMOVE_FROM_BREADCRUMB") {
+  } else if (action.type === ActionTypes.REMOVE_FROM_BREADCRUMB) {
     return {
       ...state,
       breadcrumb: state.breadcrumb.slice(0, action.payload),
     };
-  } else if (action.type === "RESET_BREADCRUMB") {
+  } else if (action.type === ActionTypes.RESET_BREADCRUMB) {
     return {
       ...state,
       breadcrumb: ["Home"],
